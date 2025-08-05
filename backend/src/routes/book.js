d5c08cd793b608b6f4fbb9246f84be0389d46197
Morpheus0208 <morpheus0208@gmail.com>
2025-08-05 16:05:13 +0200

    ajout de multer

--- backend/src/routes/book.js
+++ backend/src/routes/book.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const bookctrl = require('../controllers/book');
 const auth = require('../middlewares/auth');
+const multer = require('../../images/multer-config');
 
 const router = express.Router();
 
 // POST pour créer un livre
-router.post('/', auth, bookctrl.createBook);
+router.post('/', auth, multer, bookctrl.createBook);
 // PUT pour modifier un livre
-router.put('/:id', auth, bookctrl.updateBook);
+router.put('/:id', auth, multer, bookctrl.updateBook);
 // DELETE pour supprimer un livre
 router.delete('/:id', auth, bookctrl.deleteBook);
 // GET pour récupérer un livre par son ID
-router.get('/:id', auth, bookctrl.getBookById);
+router.get('/:id', bookctrl.getBookById);
 // GET pour récupérer tous les livres
-router.get('/', auth, bookctrl.getAllBooks);
+router.get('/', bookctrl.getAllBooks);
 
 module.exports = router;
